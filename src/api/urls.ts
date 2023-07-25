bedc0f09baa979db638d1b17aa04c8bc7ff7771f
Egor <eduaonov@gmail.com>
2023-07-25 14:38:31 +0300

    refactoring

---
+++ src/api/urls.ts
@@ -0,0 +1,4 @@
+export enum Urls {
+    ACCESS_TOKEN_API_URL = 'api/v3/clients/accesstoken',
+    BONUS_API_URL = 'api/v3/ibonus/generalinfo',
+}
--- src/pages/mainPage/chunks/bonuses/hooks/useBonusesData.tsx
+++ src/pages/mainPage/chunks/bonuses/hooks/useBonusesData.tsx
@@ -3,14 +3,15 @@ import axios from "axios";
 import {BonusesInterface} from "../Bonuses.interface";
 import {Config} from "../../../../../api/config";
 import {headers, requestData} from "../../../../../api/request";
+import {Urls} from "../../../../../api/urls";
 
 export const useBonusesData = () => {
     const [data, setData] = useState({} as BonusesInterface);
 
     const fetchBonusesData = async () => {
         try {
-            const response = await axios.post(`${Config.ACCESS_TOKEN_URL}/api/v3/clients/accesstoken`, requestData, {headers});
-            const bonusesUrl = `${Config.BONUS_URL}/api/v3/ibonus/generalinfo/${response.data.accessToken}`;
+            const response = await axios.post(`${Config.ACCESS_TOKEN_URL}/${Urls.ACCESS_TOKEN_API_URL}`, requestData, {headers});
+            const bonusesUrl = `${Config.BONUS_URL}/${Urls.BONUS_API_URL}/${response.data.accessToken}`;
             const result = await axios.get(bonusesUrl, {headers});
             setData(result.data.data);
         } catch (error) {
